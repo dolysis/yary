2146f55f2806b448ca5586160ccdc678e571d222
Bazaah <github@luxolus.com>
2021-06-05 09:14:47 +0000

    style/fmt: apply rustfmt rules

--- src/reader.rs
+++ src/reader.rs
@@ -5,11 +5,12 @@ use anyhow::bail;
 use crate::error::Result;
 
 #[derive(Debug)]
-struct Reader<I> {
-    src: I,
+struct Reader<I>
+{
+    src:    I,
     buffer: VecDeque<char>,
     column: usize,
-    mark: usize,
+    mark:   usize,
 
     reset_column: bool,
 }
@@ -18,7 +19,8 @@ impl<I> Reader<I>
 where
     I: Iterator<Item = Result<char>>,
 {
-    pub fn new(src: I) -> Self {
+    pub fn new(src: I) -> Self
+    {
         Self {
             src,
             buffer: VecDeque::new(),
@@ -30,33 +32,41 @@ where
     }
 
     /// Number of chars seen so far
-    pub fn mark(&self) -> usize {
+    pub fn mark(&self) -> usize
+    {
         self.mark
     }
 
     /// How deep into a line we currently are
-    pub fn column(&self) -> usize {
+    pub fn column(&self) -> usize
+    {
         self.column
     }
 
-    /// Reserve amt chars, returning an error if it could not reserve the
-    /// requested amount
-    pub fn reserve_exact(&mut self, amt: usize) -> Result<()> {
+    /// Reserve amt chars, returning an error if it could
+    /// not reserve the requested amount
+    pub fn reserve_exact(&mut self, amt: usize) -> Result<()>
+    {
         let reserved = self.reserve(amt)?;
 
-        if reserved != amt {
+        if reserved != amt
+        {
             bail!("only reserved {}/{} chars @{}", reserved, amt, self.mark());
         }
 
         Ok(())
     }
 
-    /// Attempt to reserve up to amt chars, returning the actual number added
-    pub fn reserve(&mut self, amt: usize) -> Result<usize> {
+    /// Attempt to reserve up to amt chars, returning the
+    /// actual number added
+    pub fn reserve(&mut self, amt: usize) -> Result<usize>
+    {
         let mut done = 0;
 
-        while done < amt {
-            match self.src.next() {
+        while done < amt
+        {
+            match self.src.next()
+            {
                 Some(Ok(c)) => self.buffer.push_front(c),
                 Some(Err(e)) => return Err(e),
                 None => return Ok(done),
@@ -68,17 +78,22 @@ where
         Ok(done)
     }
 
-    fn next_char(&mut self) -> Result<Option<char>> {
-        match self.read_one()? {
-            Some(c) => {
+    fn next_char(&mut self) -> Result<Option<char>>
+    {
+        match self.read_one()?
+        {
+            Some(c) =>
+            {
                 // We hit a line end previously, reset column size
-                if self.reset_column {
+                if self.reset_column
+                {
                     self.reset_column = false;
                     self.column = 0;
                 }
 
                 // We just hit a line end, arm column reset
-                if c == '\n' {
+                if c == '\n'
+                {
                     self.reset_column = true;
                 }
 
@@ -86,13 +101,15 @@ where
                 self.mark += 1;
 
                 Ok(Some(c))
-            }
+            },
             None => Ok(None),
         }
     }
 
-    fn read_one(&mut self) -> Result<Option<char>> {
-        match self.buffer.is_empty() {
+    fn read_one(&mut self) -> Result<Option<char>>
+    {
+        match self.buffer.is_empty()
+        {
             true => self.src.next().transpose(),
             false => Ok(self.buffer.pop_back()),
         }
@@ -105,15 +122,18 @@ where
 {
     type Item = Result<char>;
 
-    fn next(&mut self) -> Option<Self::Item> {
+    fn next(&mut self) -> Option<Self::Item>
+    {
         self.next_char().transpose()
     }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod tests
+{
     use pretty_assertions::{assert_eq, assert_ne};
+
+    use super::*;
 
     macro_rules! data {
         ($data:expr) => {
@@ -125,7 +145,8 @@ mod tests {
     }
 
     #[test]
-    fn amount() {
+    fn amount()
+    {
         let data = data!();
         let r = Reader::new(data);
 
@@ -133,7 +154,8 @@ mod tests {
     }
 
     #[test]
-    fn reserve() {
+    fn reserve()
+    {
         let data = data!();
         let mut r = Reader::new(data);
 
@@ -144,7 +166,8 @@ mod tests {
     }
 
     #[test]
-    fn reserve_partial() {
+    fn reserve_partial()
+    {
         let data = data!();
         let mut r = Reader::new(data);
 
@@ -154,7 +177,8 @@ mod tests {
     }
 
     #[test]
-    fn reserve_one() {
+    fn reserve_one()
+    {
         let data = data!();
         let mut r = Reader::new(data);
 
@@ -164,7 +188,8 @@ mod tests {
     }
 
     #[test]
-    fn reserve_zero() {
+    fn reserve_zero()
+    {
         let data = data!();
         let mut r = Reader::new(data);
 
@@ -174,14 +199,16 @@ mod tests {
     }
 
     #[test]
-    fn column() {
+    fn column()
+    {
         let data = data!("abc\nefg\nhijkl");
         let expected = vec![1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 5];
         let mut r = Reader::new(data);
 
         assert_eq!(r.column(), 0);
 
-        for indent in expected {
+        for indent in expected
+        {
             let c = r.next().unwrap().expect("impossible to error");
 
             assert_eq!(r.column(), indent, "@ char: {}, mark: {}", c, r.mark());
@@ -189,14 +216,16 @@ mod tests {
     }
 
     #[test]
-    fn mark() {
+    fn mark()
+    {
         let data = data!();
         let expected = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let mut r = Reader::new(data);
 
         assert_eq!(r.mark(), 0);
 
-        for mark in expected {
+        for mark in expected
+        {
             let c = r.next().unwrap().expect("impossible to error");
 
             assert_eq!(r.mark(), mark, "@ char: {}, mark: {}", c, r.mark());
--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,67 +1,83 @@
 use crate::token::{StreamEncoding, Token};
 
 #[derive(Debug)]
-struct Scanner<'a> {
+struct Scanner<'a>
+{
     buffer: &'a str,
-    state: StreamState,
+    state:  StreamState,
 }
 
-impl<'a> Scanner<'a> {
-    pub fn new(data: &'a str) -> Self {
+impl<'a> Scanner<'a>
+{
+    pub fn new(data: &'a str) -> Self
+    {
         Self {
             buffer: data,
-            state: StreamState::Start,
+            state:  StreamState::Start,
         }
     }
 
-    fn start_stream(&mut self) -> Option<Token<'a>> {
-        match self.state {
-            StreamState::Start => {
+    fn start_stream(&mut self) -> Option<Token<'a>>
+    {
+        match self.state
+        {
+            StreamState::Start =>
+            {
                 self.state = StreamState::Stream;
 
                 Some(Token::StreamStart(StreamEncoding::UTF8))
-            }
+            },
             _ => None,
         }
     }
 
-    fn stream_end(&mut self) -> Option<Token<'a>> {
-        match (self.state, self.buffer.is_empty()) {
+    fn stream_end(&mut self) -> Option<Token<'a>>
+    {
+        match (self.state, self.buffer.is_empty())
+        {
             (StreamState::Done, _) => None,
-            (_, true) => {
+            (_, true) =>
+            {
                 self.state = StreamState::Done;
 
                 Some(Token::StreamEnd)
-            }
+            },
             (_, false) => None,
         }
     }
 
-    fn eat_whitespace(&mut self) -> usize {
+    fn eat_whitespace(&mut self) -> usize
+    {
         let mut slice = self.buffer.char_indices().peekable();
         let mut chomped = None;
         let mut chomp_line = false;
 
-        while let Some((index, c)) = slice.next() {
-            match c {
+        while let Some((index, c)) = slice.next()
+        {
+            match c
+            {
                 // Eat spaces
-                ' ' => {}
+                ' ' =>
+                {},
                 // If we are starting a comment, chomp the entire line
                 '#' => chomp_line = true,
                 // Reset line chomp after eating one
                 '\n' => chomp_line = false,
                 // Chomp anything if we're eating the whole line
-                _ if chomp_line => {}
+                _ if chomp_line =>
+                {},
                 // We're done, encountered a character that isn't whitespace
-                _ => {
+                _ =>
+                {
                     chomped = Some(index);
                     break;
-                }
+                },
             }
         }
 
         // Adjust our buffer by the chomped length
-        if let Some(index) = chomped {
+        if let Some(index) = chomped
+        {
             self.buffer = split_at(self.buffer, index)
         }
 
@@ -69,7 +85,8 @@ impl<'a> Scanner<'a> {
         //
         // if we hit this, then we didn't get a chance to set
         // chomped in the while loop
-        if slice.peek().is_none() {
+        if slice.peek().is_none()
+        {
             chomped = self.buffer.len().into();
             self.buffer = ""
         }
@@ -77,42 +94,54 @@ impl<'a> Scanner<'a> {
         chomped.unwrap_or(0)
     }
 
-    fn document_marker(&mut self) -> Option<Token<'a>> {
-        if self.buffer.starts_with("---") {
+    fn document_marker(&mut self) -> Option<Token<'a>>
+    {
+        if self.buffer.starts_with("---")
+        {
             self.buffer = split_at(self.buffer, 3);
 
             Token::DocumentStart.into()
-        } else if self.buffer.starts_with("...") {
+        }
+        else if self.buffer.starts_with("...")
+        {
             self.buffer = split_at(self.buffer, 3);
 
             Token::DocumentEnd.into()
-        } else {
+        }
+        else
+        {
             None
         }
     }
 }
 
 #[inline(always)]
-fn split_at(b: &str, at: usize) -> &str {
+fn split_at(b: &str, at: usize) -> &str
+{
     let (_, rest) = b.split_at(at);
     rest
 }
 
-impl<'a> Iterator for Scanner<'a> {
+impl<'a> Iterator for Scanner<'a>
+{
     type Item = Token<'a>;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(begin) = self.start_stream() {
+    fn next(&mut self) -> Option<Self::Item>
+    {
+        if let Some(begin) = self.start_stream()
+        {
             return Some(begin);
         }
 
         self.eat_whitespace();
 
-        if let Some(end) = self.stream_end() {
+        if let Some(end) = self.stream_end()
+        {
             return Some(end);
         }
 
-        if let Some(document) = self.document_marker() {
+        if let Some(document) = self.document_marker()
+        {
             return Some(document);
         }
 
@@ -121,22 +150,26 @@ impl<'a> Iterator for Scanner<'a> {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum StreamState {
+enum StreamState
+{
     Start,
     Stream,
     Done,
 }
 
 #[cfg(test)]
-mod tests {
+mod tests
+{
     #[macro_use]
     mod macros;
 
-    use super::*;
     use pretty_assertions::assert_eq;
 
+    use super::*;
+
     #[test]
-    fn empty() {
+    fn empty()
+    {
         let data = "";
         let mut s = Scanner::new(data);
 
@@ -148,7 +181,8 @@ mod tests {
     }
 
     #[test]
-    fn document_markers() {
+    fn document_markers()
+    {
         let data = "\n---\n   \n...";
         let mut s = Scanner::new(data);
 
@@ -162,7 +196,8 @@ mod tests {
     }
 
     #[test]
-    fn chomp_comments() {
+    fn chomp_comments()
+    {
         let data = "  # a comment\n\n#one two three\n       #four!";
         let mut s = Scanner::new(data);
 
@@ -174,7 +209,8 @@ mod tests {
     }
 
     #[test]
-    fn comment_in_document_markers() {
+    fn comment_in_document_markers()
+    {
         let data = "---\n# abcdefg \n  # another comment     \n...";
         let mut s = Scanner::new(data);
 
