965610c3fe5fa924f4231865b0437d74678b1356
Bazaah <github@luxolus.com>
2021-06-19 10:19:30 +0000

    scanner/scalar: further improvements
    - Improved docs
    - Expand unit tests
    - Slight improvements to code

--- src/scanner/scalar/flow.rs
+++ src/scanner/scalar/flow.rs
@@ -12,18 +12,23 @@ fn scan_flow_scalar_single_quote<'b, 'c>(
 {
     let mut buffer = base;
     let mut can_borrow = true;
-    let mut start: u64;
 
     // Eat left quote
     advance!(buffer, 1);
 
-    start = 1;
-
     'scalar: loop
     {
-        // It is not legal to start a flow scalar with document
-        // start/end markers
-        if buffer.starts_with("---") || buffer.starts_with("...")
+        // Even in a scalar context, YAML prohibits starting a line
+        // with document stream tokens followed by a blank
+        // character
+        //
+        // FIXME: this is currently incorrect, we also need to check
+        // that we are at the beginning of a line which would
+        // require us tracking columns.
+        //
+        // This should be fixed once I figure out how to do that
+        if check!(~buffer => [b'-', b'-', b'-', ..] | [b'.', b'.', b'.', ..])
+            && isBlankZ!(~buffer, 3)
         {
             return Err(ScanError::InvalidFlowScalar);
         }
@@ -38,13 +43,13 @@ fn scan_flow_scalar_single_quote<'b, 'c>(
         while !isBlankZ!(~buffer)
         {
             // if we encounter an escaped quote we can no longer borrow
-            // from the backing slice, we must unescape the quote
+            // from .base, we must unescape the quote into .scratch
             if check!(~buffer => [SINGLE, SINGLE, ..])
             {
                 can_borrow = false;
 
                 scratch.push(SINGLE);
-                advance!(buffer, 2, start);
+                advance!(buffer, 2);
             }
 
             // We're done, we hit the right quote
@@ -59,51 +64,69 @@ fn scan_flow_scalar_single_quote<'b, 'c>(
                 // Safety: isBlankZ guarantees the slice is not empty
                 scratch.push(buffer.as_bytes()[0])
             }
-            advance!(buffer, 1, start);
+            advance!(buffer, 1);
         }
 
         // Consume whitespace
-        while isBlank!(~buffer) || isBreak!(~buffer)
+        loop
         {
-            if isBlank!(~buffer)
+            match (isBlank!(~buffer), isBreak!(~buffer))
             {
-                if !can_borrow
+                (false, false) => break,
+                (true, _) =>
                 {
-                    scratch.push(buffer.as_bytes()[0])
-                }
-                advance!(buffer, 1, start);
-            }
-            else
-            {
-                // need to handle potential joins
-                // e.g ===================
-                //  'a                  'a
-                //   b                   b
-                //   c
-                //   d'                  c'
-                //   -> 'a b c d'        -> 'a b \nc'
-                //
-                // Seems like the rule here is that if
-                // line consists solely of a break we add it
-                // literally, otherwise we eat blanks
-                // until we find a char
+                    if !can_borrow
+                    {
+                        scratch.push(buffer.as_bytes()[0])
+                    }
+                    advance!(buffer, 1);
+                },
+                (false, _) =>
+                {
+                    // need to handle potential joins
+                    // e.g ===================
+                    //  'a                  'a
+                    //   b                   b
+                    //   c
+                    //   d'                  c'
+                    //   -> 'a b c d'        -> 'a b \nc'
+                    //
+                    // Seems like the rule here is that if
+                    // line consists solely of a break we
+                    // add it literally,
+                    // otherwise we eat blanks
+                    // until we find a char
+                    unimplemented!(
+                        "handling of line breaks in flow scalars is not implemented yet!"
+                    )
+                },
             }
         }
     }
 
-    // Retrieve the token slice, either from the base slice, or
-    // if we couldn't borrow, the scratch space
+    // Retrieve the token slice, either from the .base slice, or
+    // if we couldn't borrow, the .scratch space
     let token = if can_borrow
     {
-        let s = (&*base).get(1..buffer.len()).unwrap();
-        let token = Token::Scalar(cow!(s), ScalarStyle::SingleQuote);
+        // Safety: we must be on a code point boundary, as the only
+        // way can get to this section is:
+        //
+        // 1. .base->0 must be a quote
+        // 2. .base->.buffer.len() - 1 must be a quote
+        // 3. .base must be valid UTF8 (its a str)
+        let fragment = base.get(1..base.len() - buffer.len()).unwrap();
+        let token = Token::Scalar(cow!(fragment), ScalarStyle::SingleQuote);
 
         Ref::Borrow(token)
     }
     else
     {
-        let s = std::str::from_utf8(scratch).unwrap();
-        let token = Token::Scalar(cow!(s), ScalarStyle::SingleQuote);
+        // Safety: characters added to scratch are either:
+        //
+        // A. added from a str (.base)
+        // B. Unescaped into valid UTF8
+        let fragment = std::str::from_utf8(scratch).unwrap();
+        let token = Token::Scalar(cow!(fragment), ScalarStyle::SingleQuote);
 
         Ref::Copy(token)
     };
@@ -120,8 +143,106 @@ fn scan_flow_scalar_single_quote<'b, 'c>(
 /// different lifetimes, specifically either a lifetime
 /// 'borrow-ed from the underlying data or 'copy-ied from
 /// some scratch space provided.
+#[derive(Debug, PartialEq)]
 pub enum Ref<'borrow, 'copy>
 {
     Borrow(Token<'borrow>),
     Copy(Token<'copy>),
+}
+
+#[cfg(test)]
+mod tests
+{
+    use anyhow::bail;
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+
+    type TestResult = anyhow::Result<()>;
+
+    #[test]
+    fn flow_single_empty() -> TestResult
+    {
+        let data = "''";
+        let scratch = &mut Vec::new();
+        let expected = Ref::Borrow(Token::Scalar(cow!(""), ScalarStyle::SingleQuote));
+
+        let (scalar, read) = scan_flow_scalar_single_quote(data, scratch)?;
+
+        assert_eq!(read, 2);
+
+        if !(scalar == expected)
+        {
+            bail!("expected\n{:?}\nbut got\n{:?}", expected, &scalar)
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn flow_single_simple() -> TestResult
+    {
+        let data = "'hello world'";
+        let scratch = &mut Vec::new();
+        let expected = Ref::Borrow(Token::Scalar(cow!("hello world"), ScalarStyle::SingleQuote));
+
+        let (scalar, read) = scan_flow_scalar_single_quote(data, scratch)?;
+
+        assert_eq!(read, 13);
+
+        if !(scalar == expected)
+        {
+            bail!("expected\n{:?}\nbut got\n{:?}", expected, &scalar)
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn flow_single_reject_document()
+    {
+        let data = ["'--- '", "'---\n'"];
+        let scratch = &mut Vec::new();
+        let expected = ScanError::InvalidFlowScalar;
+
+        for (i, &t) in (&data).into_iter().enumerate()
+        {
+            match scan_flow_scalar_single_quote(t, scratch)
+            {
+                Err(e) => assert_eq!(
+                    e, expected,
+                    "on iteration {}, expected error {}, got {}",
+                    i, expected, e
+                ),
+                Ok((unexpected, _)) => panic!(
+                    "on iteration {}, expected error {}, got unexpected value {:?}",
+                    i, expected, unexpected
+                ),
+            }
+        }
+    }
+
+    #[test]
+    fn flow_single_reject_eof()
+    {
+        let data = ["'end space ", "'", "'end word"];
+        let scratch = &mut Vec::new();
+        let expected = ScanError::UnexpectedEOF;
+
+        for (i, &t) in (&data).into_iter().enumerate()
+        {
+            match scan_flow_scalar_single_quote(t, scratch)
+            {
+                Err(e) => assert_eq!(
+                    e, expected,
+                    "on iteration {}, expected error {}, got {}",
+                    i, expected, e
+                ),
+                Ok((unexpected, _)) => panic!(
+                    "on iteration {}, expected error {}, got unexpected value {:?}",
+                    i, expected, unexpected
+                ),
+            }
+        }
+    }
 }
