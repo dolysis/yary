09fc1285454aabc1d8575af7dcd5b224f59be65d
Bazaah <github@luxolus.com>
2021-06-28 23:29:24 +0000

    scanner/tag: add scan_tag_directive, scan_node_tag
    For scanning %TAG directive handle/prefixes and node !!tag
    handle/suffixes.

--- src/scanner/tag.rs
+++ src/scanner/tag.rs
@@ -64,10 +64,182 @@
 
 use std::ops::Range;
 
-use crate::scanner::{
-    error::{ScanError, ScanResult as Result},
-    scalar::escape::tag_uri_unescape,
+use crate::{
+    scanner::{
+        eat_whitespace,
+        error::{ScanError, ScanResult as Result},
+        scalar::escape::tag_uri_unescape,
+    },
+    token::{Ref, Token},
 };
+
+/// Scan a tag directive from .base returning a tag
+/// directive token.
+///
+/// This function will attempt to borrow from .base where
+/// possible, but may also copy the directive's handle and
+/// prefix into .scratch if borrowing is not possible.
+pub(in crate::scanner) fn scan_tag_directive<'b, 'c>(
+    base: &'b str,
+    scratch: &'c mut Vec<u8>,
+) -> Result<(Ref<'b, 'c>, usize)>
+{
+    let mut buffer = base;
+    let mut can_borrow = true;
+
+    // %TAG !named! :tag:prefix # a comment\n
+    //      ^^^^^^^
+    let (handle, amt) = match scan_tag_handle(buffer)?
+    {
+        Some((handle, amt)) => (handle.into_inner(), amt),
+        None => return Err(ScanError::InvalidTagHandle),
+    };
+    advance!(buffer, amt);
+
+    // %TAG !named! :tag:prefix # a comment\n
+    //             ^
+    // Check that there is >= 1 whitespace between handle and
+    // prefix
+    isBlank!(~buffer, else ScanError::InvalidTagPrefix)?;
+
+    // Chomp whitespace to prefix
+    advance!(buffer, eat_whitespace(buffer, false));
+
+    // %TAG !named! :tag:prefix # a comment\n
+    //              ^^^^^^^^^^^
+    let (prefix, amt) = scan_tag_uri(buffer, scratch, &mut can_borrow, false)?;
+
+    // %TAG !named! tag-prefix # a comment\n
+    //                        ^
+    // Check there is whitespace or a newline after the tag
+    isWhiteSpace!(~buffer, amt, else ScanError::InvalidTagPrefix)?;
+
+    // If we can borrow, just take the range directly out of
+    // .buffer
+    let token = if can_borrow
+    {
+        Token::TagDirective(cow!(handle), cow!(&buffer[prefix])).borrowed()
+    }
+    // Otherwise, we'll need to copy both the handle and prefix, to unify our
+    // lifetimes. Note that this isn't strictly necessary, but requiring Token to
+    // contain two unrelated lifetimes is just asking for pain and suffering.
+    else
+    {
+        let start = scratch.len();
+        scratch.extend_from_slice(handle.as_bytes());
+
+        let handle = std::str::from_utf8(&scratch[start..]).unwrap();
+        let prefix = std::str::from_utf8(&scratch[prefix]).unwrap();
+
+        Token::TagDirective(cow!(handle), cow!(prefix)).copied()
+    };
+
+    advance!(buffer, amt);
+
+    Ok((token, base.len() - buffer.len()))
+}
+
+/// Scan a node's tag, returning a [Token::Tag][Token] with
+/// the tag's handle and suffix.
+///
+/// This function will attempt to borrow from .base, but may
+/// copy the handle and suffix into .scratch if borrowing is
+/// impossible.
+///
+/// Furthermore, this function returns several distinct tag
+/// patterns (handle, suffix):
+///
+/// ("", suffix) => A verbatim tag
+/// ("!", "") => A non resolving tag
+/// (handle, suffix) => A primary, secondary or named tag
+pub(in crate::scanner) fn scan_node_tag<'b, 'c>(
+    base: &'b str,
+    scratch: &'c mut Vec<u8>,
+) -> Result<(Ref<'b, 'c>, usize)>
+{
+    let mut buffer = base;
+    let mut can_borrow = true;
+
+    /*
+     * Note that the odd &buffer[0..0] is intentional.
+     *
+     * Certain crates (looking at you Bytes) abuse Rust's
+     * pointer semantics to determine if a pointer is
+     * "inside" another pointer, and str literals would
+     * violate the "inside"ness, so instead we slice
+     * a zero length sub-slice out.
+     */
+
+    // !<global:verbatim:tag:> "node"
+    // ^^
+    // If its a verbatim tag scan it
+    let (token, amt) = if check!(~buffer => [b'!', b'<', ..])
+    {
+        advance!(buffer, 2);
+
+        // !<global:verbatim:tag:> "node"
+        //   ^^^^^^^^^^^^^^^^^^^^
+        let (verbatim, amt) = scan_tag_uri(buffer, scratch, &mut can_borrow, true)?;
+
+        // !<global:verbatim:tag:> "node"
+        //                       ^
+        check!(~buffer, amt + 1 => b'>', else ScanError::InvalidTagSuffix)?;
+
+        let token = assemble_tag(buffer, scratch, &buffer[0..0], verbatim, can_borrow);
+
+        (token, amt + 1)
+    }
+    // Otherwise scan it as a normal tag
+    else
+    {
+        match scan_tag_handle(buffer)?
+        {
+            // ! "node"
+            // ^
+            // Single ! without a suffix disables tag resolution
+            Some((TagHandle::Primary(h), amt)) =>
+            {
+                (Token::Tag(cow!(h), cow!(&buffer[0..0])).borrowed(), amt)
+            },
+            // !!global "node" OR !named!global "node"
+            // ^^                 ^^^^^^^
+            // Got a secondary or named tag, scan the suffix now
+            Some((TagHandle::Secondary(h), amt)) | Some((TagHandle::Named(h), amt)) =>
+            {
+                advance!(buffer, amt);
+
+                // !!global "node" OR !named!global "node"
+                //   ^^^^^^                  ^^^^^^
+                let (suffix, amt) = scan_tag_uri(buffer, scratch, &mut can_borrow, false)?;
+
+                let token = assemble_tag(buffer, scratch, h, suffix, can_borrow);
+
+                (token, amt)
+            },
+            // !local "node"
+            // Handle scan couldn't find a closing !, meaning this is a local tag
+            None =>
+            {
+                // !local "node"
+                // ^
+                let handle = &buffer[..1];
+                advance!(buffer, 1);
+
+                // !local "node"
+                //  ^^^^^
+                let (suffix, amt) = scan_tag_uri(buffer, scratch, &mut can_borrow, false)?;
+
+                let token = assemble_tag(buffer, scratch, handle, suffix, can_borrow);
+
+                (token, amt)
+            },
+        }
+    };
+
+    advance!(buffer, amt);
+
+    Ok((token, base.len() - buffer.len()))
+}
 
 /// Scan a tag directive prefix, as defined in
 /// [Section 6.22][Link], returning a range from either
@@ -235,5 +407,31 @@ where
             Some(b) if f(b) => index += 1,
             _ => return &b[..index],
         }
+    }
+}
+
+/// Helper function for constructing
+/// [Ref][Ref]<[Token::Tag][Token]>s
+fn assemble_tag<'b, 'c>(
+    buffer: &'b str,
+    scratch: &'c mut Vec<u8>,
+    handle: &'b str,
+    suffix: Range<usize>,
+    can_borrow: bool,
+) -> Ref<'b, 'c>
+{
+    if can_borrow
+    {
+        Token::Tag(cow!(handle), cow!(&buffer[suffix])).borrowed()
+    }
+    else
+    {
+        let start = scratch.len();
+        scratch.extend_from_slice(handle.as_bytes());
+
+        let h = std::str::from_utf8(&scratch[start..]).unwrap();
+        let t = std::str::from_utf8(&scratch[suffix]).unwrap();
+
+        Token::Tag(cow!(h), cow!(t)).copied()
     }
 }
