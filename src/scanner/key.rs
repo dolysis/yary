a0e184431f4b03fe5458c67f200a0e1de1c3bb09
Bazaah <github@luxolus.com>
2021-07-07 18:56:06 +0000

    scanner/key: add, structs for managing key tokens
    This module contains the beginnings of the state required to track and
    store tokens which may be "found" out of sequence, notably when we first
    need to parse a scalar (Token #1) then check if a value sequence follows
    it (Token #2), and if so, return a key token first (Token #3), where the
    correct order of tokens is:
    
        Token #3 -> Token #1 -> Token #2
    
    We also need to track in the scanner whether a key is even possible, e.g
    if we just parsed a value token, the next scalar _is not_ a key

---
+++ src/scanner/key.rs
@@ -0,0 +1,166 @@
+use super::scalar::flow::ScalarRange;
+use crate::{
+    scanner::error::ScanResult as Result,
+    token::{Ref, Token},
+};
+
+#[derive(Debug, Clone)]
+pub(in crate::scanner) struct Key
+{
+    next:  Option<NextKey>,
+    state: Option<KeyState>,
+
+    pub mark: usize,
+    pub line: usize,
+}
+
+impl Key
+{
+    pub fn new(mark: usize, line: usize) -> Self
+    {
+        Self {
+            mark,
+            line,
+            state: None,
+            next: None,
+        }
+    }
+
+    /// A key is possible / .required at the current stream
+    /// position
+    pub fn possible(&mut self, required: bool)
+    {
+        self.next = match required
+        {
+            true => NextKey::Required,
+            false => NextKey::Possible,
+        }
+        .into()
+    }
+
+    /// A key is impossible / illegal at the current stream
+    /// position
+    pub fn impossible(&mut self)
+    {
+        self.next = Some(NextKey::Disallowed)
+    }
+
+    /// Is a key allowed at the current position?
+    pub fn allowed(&self) -> bool
+    {
+        self.next.as_ref().map(|s| s.allowed()).unwrap_or(false)
+    }
+
+    /// Is a key required at the current position?
+    pub fn required(&self) -> bool
+    {
+        self.next.as_ref().map(|s| s.required()).unwrap_or(false)
+    }
+
+    pub fn save(&mut self, r: ScalarRange, advance: usize)
+    {
+        self.state = Some(KeyState::new(r, advance))
+    }
+
+    pub fn has_tokens(&self) -> bool
+    {
+        self.state.is_some()
+    }
+
+    pub fn next_token<'b, 'c>(
+        &mut self,
+        base: &'b str,
+        scratch: &'c mut Vec<u8>,
+    ) -> Result<Option<(Ref<'b, 'c>, usize)>>
+    {
+        let state = match self.state.take()
+        {
+            Some(state) => state,
+            None => return Ok(None),
+        };
+
+        match state.next_state(base, scratch)?
+        {
+            (state @ Some(_), token, amt) =>
+            {
+                self.state = state;
+
+                Ok(Some((token, amt)))
+            },
+            (None, token, amt) => Ok(Some((token, amt))),
+        }
+    }
+}
+
+impl Default for Key
+{
+    fn default() -> Self
+    {
+        Self::new(0, 1)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub(in crate::scanner) enum NextKey
+{
+    Disallowed,
+    Possible,
+    Required,
+}
+
+impl NextKey
+{
+    fn allowed(&self) -> bool
+    {
+        matches!(self, Self::Possible | Self::Required)
+    }
+
+    fn required(&self) -> bool
+    {
+        matches!(self, Self::Required)
+    }
+}
+
+impl Default for NextKey
+{
+    fn default() -> Self
+    {
+        Self::Disallowed
+    }
+}
+
+#[derive(Debug, Clone)]
+pub(in crate::scanner) enum KeyState
+{
+    Start(ScalarRange, usize),
+    KeyYielded(ScalarRange, usize),
+}
+
+impl KeyState
+{
+    pub fn new(r: ScalarRange, advance: usize) -> Self
+    {
+        Self::Start(r, advance)
+    }
+
+    pub fn next_state<'b, 'c>(
+        self,
+        base: &'b str,
+        scratch: &'c mut Vec<u8>,
+    ) -> Result<(Option<Self>, Ref<'b, 'c>, usize)>
+    {
+        match self
+        {
+            Self::Start(r, amt) => Ok((Some(Self::KeyYielded(r, amt)), Token::Key.borrowed(), 0)),
+            Self::KeyYielded(r, amt) => Ok((None, r.into_token(base, scratch)?, amt)),
+        }
+    }
+
+    pub fn into_inner(self) -> (ScalarRange, usize)
+    {
+        match self
+        {
+            Self::Start(r, amt) | Self::KeyYielded(r, amt) => (r, amt),
+        }
+    }
+}
