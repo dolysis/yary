6880c673dd48b2e873e74a4869b3da3b070260ce
Bazaah <github@luxolus.com>
2021-09-11 05:49:43 +0000

    scanner/entry: add MaybeToken wrapper to allow for deferred tokens

--- src/scanner/entry.rs
+++ src/scanner/entry.rs
@@ -1,6 +1,9 @@
 use std::cmp::Ordering;
 
-use crate::token::Token;
+use crate::{
+    scanner::{error::ScanResult as Result, scalar::flow},
+    token::Token,
+};
 
 /// A wrapper around a token containing a custom Ord impl
 /// based on the token's position in the buffer.
@@ -10,15 +13,20 @@ use crate::token::Token;
 #[derive(Debug)]
 pub(crate) struct TokenEntry<'de>
 {
-    pub token: Token<'de>,
-    read_at:   usize,
+    pub wrap: MaybeToken<'de>,
+    read_at:  usize,
 }
 
 impl<'de> TokenEntry<'de>
 {
-    pub fn new(token: Token<'de>, read_at: usize) -> Self
+    pub fn new<T>(token: T, read_at: usize) -> Self
+    where
+        T: Into<MaybeToken<'de>>,
     {
-        Self { token, read_at }
+        Self {
+            wrap: token.into(),
+            read_at,
+        }
     }
 
     pub fn read_at(&self) -> usize
@@ -26,9 +34,14 @@ impl<'de> TokenEntry<'de>
         self.read_at
     }
 
-    pub fn into_token(self) -> Token<'de>
+    pub fn is_processed(&self) -> bool
     {
-        self.token
+        matches!(&self.wrap, MaybeToken::Token(_))
+    }
+
+    pub fn into_token(self) -> Result<Token<'de>>
+    {
+        self.wrap.into_token()
     }
 }
 
@@ -55,5 +68,82 @@ impl<'de> Ord for TokenEntry<'de>
     fn cmp(&self, other: &Self) -> Ordering
     {
         self.read_at.cmp(&other.read_at)
+    }
+}
+
+#[derive(Debug)]
+pub enum MaybeToken<'de>
+{
+    Token(Token<'de>),
+    Deferred(Lazy<'de>),
+}
+
+impl<'de> MaybeToken<'de>
+{
+    pub fn into_token(self) -> Result<Token<'de>>
+    {
+        match self
+        {
+            Self::Token(token) => Ok(token),
+            Self::Deferred(lazy) => lazy.into_token(),
+        }
+    }
+}
+
+impl<'de> From<Token<'de>> for MaybeToken<'de>
+{
+    fn from(token: Token<'de>) -> Self
+    {
+        Self::Token(token)
+    }
+}
+
+impl<'de, T> From<T> for MaybeToken<'de>
+where
+    T: Into<Lazy<'de>>,
+{
+    fn from(t: T) -> Self
+    {
+        Self::Deferred(t.into())
+    }
+}
+
+#[derive(Debug)]
+pub struct Lazy<'de>
+{
+    inner: LazyImpl<'de>,
+}
+
+impl<'de> Lazy<'de>
+{
+    pub fn into_token(self) -> Result<Token<'de>>
+    {
+        self.inner.into_token()
+    }
+}
+impl<'de> From<flow::Deferred<'de>> for Lazy<'de>
+{
+    fn from(inner: flow::Deferred<'de>) -> Self
+    {
+        Self {
+            inner: LazyImpl::ScalarF(inner),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum LazyImpl<'de>
+{
+    ScalarF(flow::Deferred<'de>),
+}
+
+impl<'de> LazyImpl<'de>
+{
+    pub fn into_token(self) -> Result<Token<'de>>
+    {
+        match self
+        {
+            Self::ScalarF(inner) => inner.into_token(),
+        }
     }
 }
