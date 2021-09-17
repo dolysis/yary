fc47938b19895ee08b34e3a96d38c6bb142dab34
Bazaah <github@luxolus.com>
2021-09-17 16:23:29 +0000

    lib/**: add MPL 2.0 header to source code

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL
+ * was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 pub type Slice<'a> = std::borrow::Cow<'a, str>;
 
 /// Tokens that may be emitted by a YAML scanner
