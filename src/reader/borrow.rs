19296bbc410ff8b0a6201e71e6d0be9e7fdf113e
Bazaah <github@luxolus.com>
2021-12-29 14:13:34 +0000

    reader/borrow: add BorrowReader
    Naive implementation of Read using an existing, borrowed UTF8
    slice (&str).

---
+++ src/reader/borrow.rs
@@ -0,0 +1,65 @@
+/*
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL
+ * was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+use super::{error::ReaderResult as Result, private, Read, Reader};
+use crate::{
+    queue::Queue,
+    scanner::{
+        entry::TokenEntry,
+        flag::{Flags, O_EXTENDABLE},
+        Scanner,
+    },
+};
+
+#[derive(Debug, Clone)]
+pub(crate) struct BorrowReader<'de>
+{
+    data: &'de str,
+}
+
+impl<'de> BorrowReader<'de>
+{
+    pub fn new(data: &'de str) -> Self
+    {
+        Self { data }
+    }
+
+    pub fn try_from_bytes(data: &'de [u8]) -> Result<Self>
+    {
+        let this = std::str::from_utf8(data).map(Self::new)?;
+
+        Ok(this)
+    }
+
+    pub fn new_reader(&'de self, opts: Flags) -> Reader<'de, Self>
+    {
+        Reader::new(self, opts)
+    }
+}
+
+impl<'a> Read for BorrowReader<'a>
+{
+    fn drive<'de>(
+        &'de self,
+        scanner: &mut Scanner,
+        queue: &mut Queue<TokenEntry<'de>>,
+        options: Flags,
+    ) -> Result<()>
+    {
+        // This implementation is never extendable, so we remove the
+        // option from the set if it exists
+        scanner.scan_tokens(options & !O_EXTENDABLE, self.data, queue)?;
+
+        Ok(())
+    }
+
+    unsafe fn consume(&self, _bound: usize) -> Result<()>
+    {
+        Ok(())
+    }
+}
+
+impl private::Sealed for BorrowReader<'_> {}
