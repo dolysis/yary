b06dc421e7dd7dc29b908d0c9c36c63aa6040c32
Bazaah <github@luxolus.com>
2022-03-18 15:15:22 +0000

    event/error: document ParseError variants

--- src/event/error.rs
+++ src/event/error.rs
@@ -4,6 +4,9 @@
  * was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+//! This module contains the errors that may surface while
+//! parsing a YAML byte stream.
+
 use std::{
     fmt::{self, Debug},
     io,
@@ -12,27 +15,177 @@ use std::{
 
 use crate::{reader::ReaderError, scanner::error::ScanError};
 
+/// Result type returned by [`yary::event`](super)
 pub type ParseResult<T> = std::result::Result<T, ParseError>;
 
+/// Possible errors that can be encountered while parsing
+/// YAML events.
 #[derive(Debug)]
 pub enum ParseError
 {
+    /// A mismatch between the stream's actual state and
+    /// what the parser is expecting occurred.
+    ///
+    /// Typically, this only happens if a [`Read`] source is
+    /// given to two separate parsers.
+    ///
+    /// [`Read`]: trait@crate::reader::Read
     CorruptStream,
+
+    /// More than one `%YAML` directive was found inside a
+    /// single document's context.
+    ///
+    /// ## Examples
+    ///
+    /// ```yaml
+    /// ---
+    /// %YAML 1.2
+    /// %YAML 1.2
+    /// #^^^^^^^^ DuplicateVersion
+    /// ```
     DuplicateVersion,
+
+    /// More than one `%TAG` directive was found _for the
+    /// same handle_ inside a single document's context.
+    ///
+    /// ## Examples
+    ///
+    /// ```yaml
+    /// ---
+    /// %TAG !handle! my:custom:tag/
+    /// %TAG !handle! my:other:tag/
+    /// #    ^^^^^^^^ DuplicateTagDirective
+    /// ```
     DuplicateTagDirective,
+
+    /// A tag referenced a handle that has not been defined.
+    ///
+    /// ## Examples
+    ///
+    /// ```yaml
+    /// ---
+    /// key: !handle! value
+    /// #    ^^^^^^^^ UndefinedTag
+    /// ```
     UndefinedTag,
+
+    /// In certain cases YAML requires an indication that
+    /// another document is being started, necessitating
+    /// a DocumentStart '---' symbol.
+    ///
+    /// Most commonly, if a stream contains two documents
+    /// the first must include a DocumentEnd ('...') symbol,
+    /// or the second must start with a DocumentStart.
+    ///
+    /// ## Examples
+    ///
+    /// ```yaml
+    /// document: 1
+    /// # <-- either a '...' or '---' is required here
+    /// # ^^^^^ MissingDocumentStart
+    /// %YAML 1.2
+    /// document: 2
+    /// ```
     MissingDocumentStart,
+
+    /// A entry in a block sequence was required but not
+    /// found in the stream
+    ///
+    /// ## Examples
+    ///
+    /// ```yaml
+    /// - 1
+    /// - 2
+    /// - 3
+    /// foo: bar # should be: '- foo: bar'
+    /// #^^ MissingBlockEntry
+    /// ```
     MissingBlockEntry,
+
+    /// A YAML node was required but not found.
+    ///
+    /// YAML tends to be very forgiving with missing nodes,
+    /// but it is possible to construct a YAML stream with a
+    /// required Node, most commonly if using tags or
+    /// anchors.
+    ///
+    /// ## Examples
+    ///
+    /// ```yaml
+    /// tag: !!str      # wants: !!str 'my tagged scalar'
+    /// #    ^^^^^ MissingNode
+    ///
+    /// anchor: &anchor # wants: &anchor 'my anchored scalar'
+    /// #       ^^^^^^^ MissingNode
+    /// ```
     MissingNode,
+
+    /// A YAML mapping key was required but not found.
+    ///
+    /// Frequently caused by poor indentation in YAML
+    /// documents.
+    ///
+    /// ```yaml
+    /// nested:
+    ///     key: value
+    ///     bad: # YAML requires that a value exist on the same line as its key
+    /// #   ^^^^ MissingKey
+    ///          value with a different line to key
+    /// ```
+    ///
+    /// ```yaml
+    /// some very long key over 1024 bytes long...: value
+    /// # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ MissingKey
+    /// ```
     MissingKey,
+
+    /// A YAML flow sequence was missing a terminus (']') or
+    /// continuation (',') symbol.
+    ///
+    /// ## Examples
+    ///
+    /// ```yaml
+    /// missing terminus: [1, 2, 3
+    /// #                         ^ MissingFlowSequenceEntryOrEnd
+    /// #                           No terminus bracket closing out the sequence
+    /// ```
+    ///
+    /// ```yaml
+    /// missing continuation: [ {key1: value} {key2: value} ]
+    /// #                                    ^ MissingFlowSequenceEntryOrEnd
+    /// ```
     MissingFlowSequenceEntryOrEnd,
+
+    /// A YAML flow mapping was missing a terminus ('}') or
+    /// continuation (',') symbol.
+    ///
+    /// ## Examples
+    /// ```yaml
+    /// missing terminus: {key1: value, key2: value
+    /// #                                          ^ MissingFlowMappingEntryOrEnd
+    /// #                                            No terminus bracket closing out the mapping
+    /// ```
+    ///
+    /// ```yaml
+    /// missing continuation: {first: value second: value}
+    /// #                                  ^ MissingFlowMappingEntryOrEnd
+    /// ```
     MissingFlowMappingEntryOrEnd,
 
-    Scanner(ScanError),
-    UTF8(Utf8Error),
-    IO(io::Error),
-
+    /// The parser was expecting more tokens, but the byte
+    /// stream finished unexpectedly.
     UnexpectedEOF,
+
+    /// A issue occurred during stream scanning.
+    Scanner(ScanError),
+
+    /// A UTF8 encoded byte stream encountered an encoding
+    /// error.
+    UTF8(Utf8Error),
+
+    /// An IO error occurred while attempting to read the
+    /// byte stream.
+    IO(io::Error),
 }
 
 impl From<ScanError> for ParseError
