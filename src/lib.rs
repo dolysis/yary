a8230f86f26ff3a5e26284582a1f7367c85f93c0
Bazaah <github@luxolus.com>
2022-03-18 15:24:38 +0000

    lib: stub library documentation

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,17 @@
  * was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+//! This library exposes methods for interacting with YAML
+//! byte streams.
+//!
+//! It is currently still in development, and will likely
+//! have multiple breaking changes to the exposed API before
+//! stabilizing. Use at your own risk.
+//!
+//! The exposed APIs are grouped by module, and no high
+//! level API yet exists for this library, though this will
+//! change in the future.
+
 #![allow(dead_code)]
 #![allow(clippy::suspicious_else_formatting)]
 
