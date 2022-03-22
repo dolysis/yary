519482f79828d8d91e96f81457263560e47cde50
Bazaah <github@luxolus.com>
2022-03-22 17:28:02 +0000

    lib/reader: add error conversion to ErrorKind

--- src/reader/error.rs
+++ src/reader/error.rs
@@ -4,16 +4,28 @@
  * was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+//! Error types returned from the [`yary::reader`](super)
+//! module.
+
 use std::{error::Error as StdError, fmt, io, str::Utf8Error};
 
-use crate::scanner::error::ScanError;
+use crate::{
+    error::internal::{ErrorCode, ErrorKind, SourceError},
+    scanner::error::ScanError,
+};
 
+/// Type alias of the `Result`s returned from this module
 pub type ReaderResult<T> = std::result::Result<T, ReaderError>;
 
+/// Possible errors that can occur while reading from YAML
+/// byte streams
 #[derive(Debug)]
 pub enum ReaderError
 {
+    /// Encountered invalid an UTF8 sequence
     UTF8(Utf8Error),
+    /// Catch all wrapper for any underlying IO errors
+    /// reported to us
     IO(io::Error),
     Scanner(ScanError),
 }
@@ -60,5 +72,18 @@ impl From<ScanError> for ReaderError
     fn from(e: ScanError) -> Self
     {
         Self::Scanner(e)
+    }
+}
+
+impl From<ReaderError> for ErrorKind
+{
+    fn from(err: ReaderError) -> Self
+    {
+        match err
+        {
+            ReaderError::UTF8(e) => SourceError::UTF8(e).into(),
+            ReaderError::IO(e) => SourceError::IO(e).into(),
+            ReaderError::Scanner(e) => ErrorCode::from(e).into(),
+        }
     }
 }
