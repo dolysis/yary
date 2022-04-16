5df8cf5cefe46004f1d4c9867f44814d4dd3618e
Bazaah <github@luxolus.com>
2022-04-16 13:51:35 +0000

    node/visitor: add module skeleton

--- src/node/visitor/mod.rs
+++ src/node/visitor/mod.rs
@@ -3,13 +3,3 @@
  * Mozilla Public License, v. 2.0. If a copy of the MPL
  * was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
-//! This module contains the basis for storing arbitrary
-//! YAML documents as an in memory graph.
-
-use crate::event::types::Slice;
-
-pub(crate) mod error;
-mod graph;
-mod nodes;
-mod visitor;
